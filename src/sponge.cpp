28fc4dc72f9fd40e56a1f4a2a935cc72be5d21e5
Alombaros <Alombaros@gmail.com>
2021-04-16 13:04:48 +0200

    reformat + tiny bug fix

--- src/sponge.cpp
+++ src/sponge.cpp
@@ -1,116 +1,116 @@
 #include <iostream>
 #include "../headers/sponge.h"
 
-std::vector<uint8_t> frontFaceIndices = {0, 4, 3,
-                                         4, 7, 3,
-                                         4, 8, 5,
-                                         8, 9, 5,
-                                         6, 10, 7,
-                                         10, 11, 7,
-                                         8, 12, 11,
-                                         12, 15, 11,};
+std::vector<uint8_t> frontFaceIndices = { 0,  3,  4,
+                                          4,  3,  7,
+                                          4,  5,  8,
+                                          8,  5,  9,
+                                          6,  7, 10,
+                                         10,  7, 11,
+                                          8, 11, 12,
+                                         12, 11, 15};
 
-std::vector<uint8_t> topFaceIndices = {48, 32, 51,
-                                       32, 35, 51,
-                                       32, 16, 33,
-                                       16, 17, 33,
-                                       34, 18, 35,
-                                       18, 19, 35,
-                                       16, 0, 19,
-                                       0, 3, 19,};
+std::vector<uint8_t> topFaceIndices = {48, 51, 32,
+                                       32, 51, 35,
+                                       32, 33, 16,
+                                       16, 33, 17,
+                                       34, 35, 18,
+                                       18, 35, 19,
+                                       16, 19,  0,
+                                        0, 19,  3};
 
-std::vector<uint8_t> rightFaceIndices = {3, 7, 51,
-                                         7, 55, 51,
-                                         7, 11, 23,
-                                         11, 27, 23,
-                                         39, 43, 55,
-                                         43, 59, 55,
-                                         11, 15, 59,
-                                         15, 63, 59,};
+std::vector<uint8_t> rightFaceIndices = { 3, 51,  7,
+                                          7, 51, 55,
+                                          7, 23, 11,
+                                         11, 23, 27,
+                                         39, 55, 43,
+                                         43, 55, 59,
+                                         11, 59, 15,
+                                         15, 59, 63};
 
-std::vector<uint8_t> bottomFaceIndices = {12, 28, 15,
-                                          28, 31, 15,
-                                          28, 44, 29,
-                                          44, 45, 29,
-                                          30, 46, 31,
-                                          46, 47, 31,
-                                          44, 60, 47,
-                                          60, 63, 47,};
+std::vector<uint8_t> bottomFaceIndices = {12, 15, 28,
+                                          28, 15, 31,
+                                          28, 29, 44,
+                                          44, 29, 45,
+                                          30, 31, 46,
+                                          46, 31, 47,
+                                          44, 47, 60,
+                                          60, 47, 63};
 
-std::vector<uint8_t> leftFaceIndices = {48, 52, 0,
-                                        52, 4, 0,
-                                        52, 56, 36,
-                                        56, 40, 36,
-                                        20, 24, 4,
-                                        24, 8, 4,
-                                        56, 60, 8,
-                                        60, 12, 8,};
+std::vector<uint8_t> leftFaceIndices = {48,  0, 52,
+                                        52,  0,  4,
+                                        52, 36, 56,
+                                        56, 36, 40,
+                                        20,  4, 24,
+                                        24,  4,  8,
+                                        56,  8, 60,
+                                        60,  8, 12};
 
-std::vector<uint8_t> backFaceIndices = {60, 56, 63,
-                                        56, 59, 63,
-                                        56, 52, 57,
-                                        52, 53, 57,
-                                        58, 54, 59,
-                                        54, 55, 59,
-                                        52, 48, 55,
-                                        48, 51, 55,};
+std::vector<uint8_t> backFaceIndices = {60, 63, 56,
+                                        56, 63, 59,
+                                        56, 57, 52,
+                                        52, 57, 53,
+                                        58, 59, 54,
+                                        54, 59, 55,
+                                        52, 55, 48,
+                                        48, 55, 51};
 
 std::vector<std::vector<uint8_t> *> faceIndicesList = {&frontFaceIndices, &topFaceIndices, &rightFaceIndices,
                                                        &bottomFaceIndices, &leftFaceIndices, &backFaceIndices};
 
-std::vector<uint8_t> innerParts = {5, 9, 21,
-                                   9, 25, 21,
-                                   9, 10, 25,
-                                   10, 26, 25,
-                                   10, 6, 26,
-                                   6, 22, 26,
-                                   6, 5, 22,
-                                   5, 21, 22,
+std::vector<uint8_t> innerParts = { 5, 21,  9,
+                                    9, 21, 25,
+                                    9, 25, 10,
+                                   10, 25, 26,
+                                   10, 26,  6,
+                                    6, 26, 22,
+                                    6, 22,  5,
+                                    5, 22, 21,
 
-                                   23, 27, 22,
-                                   27, 26, 22,
-                                   27, 43, 26,
-                                   43, 42, 26,
-                                   43, 39, 42,
-                                   39, 38, 42,
-                                   39, 23, 38,
-                                   23, 22, 38,
+                                   23, 22, 27,
+                                   27, 22, 26,
+                                   27, 26, 43,
+                                   43, 26, 42,
+                                   43, 42, 39,
+                                   39, 42, 38,
+                                   39, 38, 23,
+                                   23, 38, 22,
 
-                                   29, 45, 25,
-                                   45, 41, 25,
-                                   45, 46, 41,
-                                   46, 42, 41,
-                                   46, 30, 42,
-                                   30, 26, 42,
-                                   30, 29, 26,
-                                   29, 25, 26,
+                                   29, 25, 45,
+                                   45, 25, 41,
+                                   45, 41, 46,
+                                   46, 41, 42,
+                                   46, 42, 30,
+                                   30, 42, 26,
+                                   30, 26, 29,
+                                   29, 26, 25,
 
-                                   57, 53, 41,
-                                   53, 37, 41,
-                                   53, 54, 37,
-                                   54, 38, 37,
-                                   54, 58, 38,
-                                   58, 42, 38,
-                                   58, 57, 42,
-                                   57, 41, 42,
+                                   57, 41, 53,
+                                   53, 41, 37,
+                                   53, 37, 54,
+                                   54, 37, 38,
+                                   54, 38, 58,
+                                   58, 38, 42,
+                                   58, 42, 57,
+                                   57, 42, 41,
 
-                                   36, 40, 37,
-                                   40, 41, 37,
-                                   40, 24, 41,
-                                   24, 25, 41,
-                                   24, 20, 25,
-                                   20, 21, 25,
-                                   20, 36, 21,
-                                   36, 37, 21,
+                                   36, 37, 40,
+                                   40, 37, 41,
+                                   40, 41, 24,
+                                   24, 41, 25,
+                                   24, 25, 20,
+                                   20, 25, 21,
+                                   20, 21, 36,
+                                   36, 21, 37,
 
-                                   33, 17, 37,
-                                   17, 21, 37,
-                                   17, 18, 21,
-                                   18, 22, 21,
-                                   18, 34, 22,
-                                   34, 38, 22,
-                                   34, 33, 38,
-                                   33, 37, 38,};
+                                   33, 37, 17,
+                                   17, 37, 21,
+                                   17, 21, 18,
+                                   18, 21, 22,
+                                   18, 22, 34,
+                                   34, 22, 38,
+                                   34, 38, 33,
+                                   33, 38, 37};
 
 
 enum Faces {
@@ -144,8 +144,10 @@ void addFaces(uint64_t size, vector<uint32_t> &indices, const vector<Faces> &app
     }
 }
 
-bool contains(const vector<Faces> &vector, Faces &element) {
-    return any_of(vector.begin(), vector.end(), [&element](Faces face){ return face == element; });
+bool contains(const vector<Faces> &vector, Faces element) {
+    return any_of(vector.begin(), vector.end(), [&element](Faces face){
+        return face == element;
+    });
 }
 
 float getPointAbscissa(const vector<float> &sourceVector, uint32_t pointIndex) {
@@ -255,8 +257,7 @@ static void subdivideParallelepiped(const vector<float> &parallelepiped, vector<
     * Those quadrilateral are parallels and equidistant */
     for (uint8_t i = 1; i < 3; ++i) {
         float upLeftCornerX, upLeftCornerY, upLeftCornerZ;
-        upLeftCornerX = parallelepiped[0] - (parallelepiped[0] - parallelepiped[12]) * (float) i /
-                                            3.0f; //todo replace with getter functions
+        upLeftCornerX = parallelepiped[0] - (parallelepiped[0] - parallelepiped[12]) * (float) i / 3.0f; //todo replace with getter functions
         upLeftCornerY = parallelepiped[1] - (parallelepiped[1] - parallelepiped[13]) * (float) i / 3.0f;
         upLeftCornerZ = parallelepiped[2] - (parallelepiped[2] - parallelepiped[14]) * (float) i / 3.0f;
 
@@ -276,9 +277,9 @@ static void subdivideParallelepiped(const vector<float> &parallelepiped, vector<
         downRightCornerZ = parallelepiped[11] - (parallelepiped[11] - parallelepiped[23]) * (float) i / 3.0f;
 
         vector<float> quadrilateral = {
-                upLeftCornerX, upLeftCornerY, upLeftCornerZ,
-                upRightCornerX, upRightCornerY, upRightCornerZ,
-                downLeftCornerX, downLeftCornerY, downLeftCornerZ,
+                   upLeftCornerX,    upLeftCornerY,    upLeftCornerZ,
+                  upRightCornerX,   upRightCornerY,   upRightCornerZ,
+                 downLeftCornerX,  downLeftCornerY,  downLeftCornerZ,
                 downRightCornerX, downRightCornerY, downRightCornerZ,
         };
         subdivideQuadrilateral(quadrilateral, result);
@@ -315,7 +316,6 @@ void subdivideChild(uint8_t depth, vector<float> &vertices, vector<uint32_t> &in
     /* Subdivide the child parallelepiped */
     recursiveSubdivide(depth - 1, childParallelepiped, vertices, indices, childApparentFaces);
 }
-
 
 void recursiveSubdivide(uint8_t depth, const vector<float> &parallelepiped, vector<float> &vertices,
                         vector<uint32_t> &indices, const vector<Faces> &parentApparentFaces) {
